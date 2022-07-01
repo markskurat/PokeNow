efb189b78eeb0c3fd368c4b9a99644540e0501ec
markskurat <52984381+markskurat@users.noreply.github.com>
2022-07-01 15:36:27 -0400

    Initial Commit

---
+++ PokeNow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeNow
+//
+//  Created by Mark Skurat on 7/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeNow/PokeNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeNowApp.swift
+//  PokeNow
+//
+//  Created by Mark Skurat on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
